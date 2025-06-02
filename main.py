cb37c5101877649d94f4a899ab677549abb87fc8
B7AGUI <brandon.aguirre@cua.uam.mx>
2025-06-01 21:13:40 -0600

    Commit4

---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+
+def saludar():
+    nombre = entrada.get()
+    etiqueta_resultado.config(text=f"¡Hola, {nombre}!")
+
+# Crear ventana
+ventana = tk.Tk()
+ventana.title("Saludo personalizado")
+ventana.geometry("300x150")
+ventana2 = tk.TK()
+ventana2.title("Saludo personalizado")
+ventana2.geometry("300x150")
+
+# Etiqueta y campo de entrada
+etiqueta = tk.Label(ventana, text="¿Cómo te llamas?")
+etiqueta.pack(pady=5)
+
+
+
+entrada = tk.Entry(ventana)
+entrada.pack(pady=5)
+
+# Botón de saludo
+boton = tk.Button(ventana, text="Saludar", command=saludar)
+boton.pack(pady=5)
+
+# Etiqueta para mostrar el saludo
+etiqueta_resultado = tk.Label(ventana, text="")
+etiqueta_resultado.pack(pady=10)
+
+# Iniciar la ventana
+ventana.mainloop()
+
